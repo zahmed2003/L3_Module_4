9a5143acd026a66fd7fbe5add7c30d2ec28eda0a
DaveDLeague <dave.dunn@jointheleague.org>
2017-11-09 16:17:07 -0800

    Update BruteForceCracker.java

--- src/threads/BruteForceCracker.java
+++ src/threads/BruteForceCracker.java
@@ -1,5 +1,5 @@
-// Below is a completed program that uses brute force to crack a 7-digit password. 
-// On average, the program takes about 125 seconds to find the password.
+// Below is a completed program that uses brute force to crack a 9-digit password. 
+// On average, the program takes about 0.125 seconds to find the password.
 // Your goal is to re-write the algorithm using Threads. Try to shorten the time it takes 
 // to find the code to be as little as possible.
 // Let's see who can crack the code the fastest!
@@ -7,13 +7,14 @@
 import java.util.Random;
 
 public class BruteForceCracker {
-	static int code = new Random().nextInt(9999999);
+	static long code = (long)(new Random().nextDouble() * 1_000_000_000);
 	
 	static long startTime;
 	static long endTime;
 	static float elapsedTime;
 	
 	public static void main(String[] args) {
+		System.out.println(code);
 		System.out.println("Starting Brute Force Checker");
 		startTime = System.currentTimeMillis();
 		
@@ -26,13 +27,10 @@ public class BruteForceCracker {
 		System.out.println("Total time taken: " + elapsedTime + " seconds");
 	}
 	
-	public static boolean checkCode(int p){
-		System.out.print("checking code: " + p + " -- ");
+	public static boolean checkCode(long p){
 		if(p == code){
-			System.out.println("SUCCESS");
 			return true;
 		}else{
-			System.out.println("FAIL");
 			return false;
 		}
 	}
